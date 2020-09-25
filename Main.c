c3a90975d69133781faa67616941a98550535496
NimComPoo-04 <bhattacharyyaaanjishnu.04@gmail.com>
2020-09-25 06:34:45 +0100

    Made a small cat clone

--- Main.c
+++ Main.c
@@ -1,9 +1,31 @@
+// this is for file io and stuff
 #include <stdio.h>
+// this is for malloc
+#include <stdlib.h>
 
+// a simple implementation of cat
 int main(int argc, char** args)
 {
-	for( int i = 0 ; i < argc ; i++)
+	if( argc == 1) // handles a base case problem of none
 	{
-		puts(args[i]);
+		for( int i = 0 ; i < argc ; i++)
+			puts(args[i]);
+		return 0 ; 
 	}
+	
+	// this is the actual stuff
+	FILE* f = fopen(args[1], "r");
+	fseek(f, 0, SEEK_END);
+	int siz = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	// this is the main loop
+	char c = 0 ; 
+	for( int i = 0 ; i < siz ; i++)
+	{
+		c = fgetc(f);
+		printf("%c",c);
+	}
+	fclose(f);
+	return 0 ;
 }
